efe6c1f8e5344fda3d3daabe31f2083af4ed6ddb
NekoYo1 <kostyafear203@gmail.com>
2022-10-29 13:07:38 +1000

    Добавьте файлы проекта.

---
+++ WPFFILIPPOV_PR6/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFFILIPPOV_PR6
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                if (!datePicker1.SelectedDate.HasValue || !datePicker2.SelectedDate.HasValue)
+                {
+                    textBlock1.Text = "Select dates";
+                    return;
+                }
+
+                DateTime start = datePicker1.SelectedDate.Value.Date;
+                DateTime finish = datePicker2.SelectedDate.Value.Date;
+                TimeSpan difference = finish.Subtract(start);
+                textBlock1.Text = difference.TotalDays.ToString();
+            }
+        }
+    }
+}
